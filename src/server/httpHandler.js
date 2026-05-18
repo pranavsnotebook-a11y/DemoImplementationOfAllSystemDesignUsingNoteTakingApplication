a6c00d2be5c67a4727e4eb9facd77c3b7facb048
Pranav Krishna <pranav@example.com>
2026-05-18 14:14:54 +0530

    restore accidentally deleted httpHandler

---
+++ src/server/httpHandler.js
@@ -0,0 +1,36 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+export async function handleHttpRequest(req, res) {
+    let filePath = './public' + req.url;
+    if (req.url === '/') {
+        filePath = './public/index.html';
+    } else if (req.url.startsWith('/src/')) {
+        filePath = '.' + req.url;
+    }
+
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    try {
+        const content = await fs.readFile(filePath);
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content, 'utf-8');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(500);
+            res.end('Server error: ' + error.code);
+        }
+    }
+}
